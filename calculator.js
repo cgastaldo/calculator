49dad045da1c2cfa8dc798112efbbb3433cb8921
Chris Gastaldo <chris.gastaldo@gmail.com>
2024-08-18 16:21:47 -0400

    Extended eventlistener to 0-9 and operators. Started Equal functionality. Updated display to have two functions

--- calculator.js
+++ calculator.js
@@ -41,20 +41,72 @@ function operate(num1, operator, num2){
     }
 }
 
-const displayReading = document.getElementById("display");
+
 //displayReading.innerText ='This';
 
-function display(button){
-    if (typeof(button) == 'number'){
-        displayReading.innerText =button;
-    } 
+let calcArr=[];
+
+const operators ='+-*÷';
+let displayValue = document.getElementById("display")
+
+function dsplyNumber(button){
+    if (displayValue.textContent.length <5){
+        displayValue.innerText += button;
+    }
+    console.log(displayValue.textContent, typeof(displayValue))
 }
 
-const values =[]
+function dsplyOperator(button){
+    if (operators.includes(displayValue.innerText)){
+        displayValue.innerText = button;
+    }
+    else if(typeof(Number(displayValue.textContent)) === 'number'){
+        console.log(typeof(Number(displayValue.textContent)) === 'number')
+        calcArr.push(Number(displayValue.textContent));
+        displayValue.innerText = button;
+    }
+}
+
+document.getElementById("btn0").addEventListener("click", ()=> dsplyNumber(0));
+document.getElementById("btn1").addEventListener("click", ()=> dsplyNumber(1));
+document.getElementById("btn2").addEventListener("click", ()=> dsplyNumber(2));
+document.getElementById("btn3").addEventListener("click", ()=> dsplyNumber(3));
+document.getElementById("btn4").addEventListener("click", ()=> dsplyNumber(4));
+document.getElementById("btn5").addEventListener("click", ()=> dsplyNumber(5));
+document.getElementById("btn6").addEventListener("click", ()=> dsplyNumber(6));
+document.getElementById("btn7").addEventListener("click", ()=> dsplyNumber(7));
+document.getElementById("btn8").addEventListener("click", ()=> dsplyNumber(8));
+document.getElementById("btn9").addEventListener("click", ()=> dsplyNumber(9));
+
+document.getElementById("btnAdd").addEventListener("click", ()=> dsplyOperator('+'));
+document.getElementById("btnSub").addEventListener("click", ()=> dsplyOperator('-'));
+document.getElementById("btnMult").addEventListener("click", ()=> dsplyOperator('*'));
+document.getElementById("btnDiv").addEventListener("click", ()=> dsplyOperator('÷'));
 
 
 
-//document.getElementById("btn1").addEventListener("click", ()=> values.push(1));
-document.getElementById("btn1").addEventListener("click", function(){
-    display(1)
+
+//extend function into other operators
+//add same logic to operator to number
+//log when push equal
+
+document.getElementById("btnEqual").addEventListener("click", function(){
+    displayValue = document.getElementById("display")
+    calcArr = [24, '+', 12];
+    if (calcArr[1] === '+'){
+        displayValue.innerText = add(calcArr[0], calcArr[2]);
+    } 
+    else if (calcArr[1] === '-'){
+        displayValue.innerText = subtract(calcArr[0], calcArr[2]);
+    } 
+    else if (calcArr[1] === '*'){
+        displayValue.innerText = multiply(calcArr[0], calcArr[2]);
+    } 
+    else if (calcArr[1] === '÷'){
+        displayValue.innerText = divide(calcArr[0], calcArr[2]);
+    } 
+    else{
+        displayValue.innerText = calcArr[0];
+    }
+
 })
